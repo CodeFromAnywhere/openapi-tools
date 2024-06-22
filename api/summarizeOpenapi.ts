e5a0f1ef3ae23ca1c847bfa06407e276fb75625c
Wijnand Karsens <wijnand@karsens.com>
2024-06-22 20:12:09 +0200

    improvements

--- api/summarizeOpenapi.ts
+++ api/summarizeOpenapi.ts
@@ -1,3 +1,6 @@
+import { notEmpty } from "from-anywhere";
+import { getOpenapiOperations } from "openapi-util";
+
 /**
 Serverless function to get all operationId/summary pairs for any openapi in text or json
 */
@@ -6,7 +9,45 @@ export const GET = async (request: Request) => {
   const openapiUrl = url.pathname.split("/").slice(2).join("/");
   const isJson = request.headers.get("accept") === "application/json";
 
+  const openapiId = openapiUrl;
+  const openapiDetails =
+    openapiId && openapiUrl
+      ? await getOpenapiOperations(openapiId, openapiUrl)
+      : undefined;
+
+  if (!openapiDetails) {
+    return;
+  }
+
+  const { tags } = openapiDetails;
+
+  const llmString = tags
+    .concat({ name: "__undefined", description: "No tags present" })
+    .map((tag) => {
+      const description = tag.description
+        ? `: ${tag.description}`
+        : tag.externalDocs
+        ? `: ${tag.externalDocs.url}`
+        : "";
+
+      const filtered = openapiDetails.operations.filter((x) =>
+        tag.name === "__undefined"
+          ? !x.operation.tags?.length
+          : x.operation.tags?.includes(tag.name),
+      );
+
+      return filtered.length === 0
+        ? null
+        : `${tag.name}${description}\n${filtered
+            .map((item) => {
+              return `- ${item.id} - ${item.operation.summary}`;
+            })
+            .join("\n")}`;
+    })
+    .filter(notEmpty)
+    .join("\n\n");
+
   //use stuff from explorer I already made
 
-  return new Response("Hello, World!");
+  return new Response(llmString);
 };
